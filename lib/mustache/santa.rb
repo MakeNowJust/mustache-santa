a2b319fb703f4bccf932144a07585946f902fec9
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-02-10 17:52:46 +0900

    chore: bundle gem mustache-santa

---
+++ lib/mustache/santa.rb
@@ -0,0 +1,8 @@
+require "mustache/santa/version"
+
+module Mustache
+  module Santa
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/mustache/santa/version.rb
@@ -0,0 +1,5 @@
+module Mustache
+  module Santa
+    VERSION = "0.1.0"
+  end
+end
